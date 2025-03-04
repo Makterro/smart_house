7ae320200d28a35a2259ac9e0b1aa012e798318a
Makterro <makkrs@mail.ru>
2025-03-04 12:33:05 +0700

    fix config

--- app/core/config.py
+++ app/core/config.py
@@ -11,7 +11,12 @@ class Settings(BaseSettings):
     MINIO_ENDPOINT: str = "http://minio:9000"
     MINIO_ACCESS_KEY: str = "minioadmin"  # значение по умолчанию
     MINIO_SECRET_KEY: str = "minioadmin"  # значение по умолчанию
-    
+    REDIS_URL: str = 'redis://redis:6379'
+    MINIO_ROOT_USER: str = 'minioadmin'
+    MINIO_ROOT_PASSWORD: str = 'minioadmin'
+    MINIO_ENDPOINT: str = 'minio:9000'
+
+
     # Пути
     MEDIA_DIR: str = "media"
 
